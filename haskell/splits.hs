b6bfc411c25544915111ab37705fd7afc4925eaa
terhorst <terhorst@gmail.com>
2010-12-21 15:37:10 -0800

    memoization

--- haskell/splits.hs
+++ haskell/splits.hs
@@ -1,10 +1,17 @@
+import qualified Data.MemoCombinators as Memo
+import qualified Data.IntSet as IntSet
 import qualified Data.Set as Set
 import List (inits, tails, delete)
+import Control.Parallel.Strategies
+import Control.Parallel
 
 sf s = Set.fromList s
 st s = Set.toList s
 
-type Block = Set.Set Int
+isf s = IntSet.fromList s
+ist s = IntSet.toList s
+
+type Block = IntSet.IntSet
 type Split = Set.Set Block
 type SplitSystem = Set.Set Split
 
@@ -21,27 +28,35 @@ cartProd (set:sets) = let cp = cartProd sets in [x:xs | x <- set, xs <- cp]
 cartProd [] = [[]]
 
 makeSplit :: Int -> [Int] -> Split
-makeSplit n block = sf [a, Set.difference (sf [1..n]) a] where a = sf block 
+makeSplit n block = sf [a, IntSet.difference (isf [1..n]) a] where a = isf block 
 
-weaklyCompatibleTriple :: Set.Set Block -> Bool
-weaklyCompatibleTriple ss = (Set.null $ foldr1 (Set.intersection) ss') ||
-		(any (Set.null . foldl1 Set.difference) $ permutationsOf ss')
+wct :: Set.Set Block -> Bool
+wct ss = (IntSet.null $ foldr1 (IntSet.intersection) ss') ||
+		(any (IntSet.null . foldl1 IntSet.difference) $ permutationsOf ss')
 		where ss' = st ss
+weaklyCompatibleTriple = (Memo.wrap toTriple fromTriple memoString) wct where
+	memoString = Memo.list Memo.char
+	toTriple str = (read str) :: Set.Set Block
+	fromTriple trip = show trip
+
+ctrp :: [Split] -> Bool
+ctrp triple = all (weaklyCompatibleTriple) [ sf lst | lst <- cartProd $ map (st) triple ]
+checkTriple = (Memo.wrap toTriple fromTriple memoString) ctrp where
+	memoString = Memo.list Memo.char
+	toTriple str = (read str) :: [Split]
+	fromTriple trip = show trip
 
 isWeaklyCompatible :: SplitSystem -> Bool
-isWeaklyCompatible ss =
-	all (weaklyCompatibleTriple) [ sf lst |
-		triple <- combinationsOf 3 $ st ss,
-		lst <- cartProd $ map (st) triple ]
+isWeaklyCompatible ss = all (checkTriple) (combinationsOf 3 $ st ss)
 
 isCircular :: SplitSystem -> Bool
 isCircular ss = isWeaklyCompatible ss' where
 	ss' = Set.union ss $ sf [ mergeSplits s1 s2 |
 		(s1:s2:_) <- combinationsOf 2 $ st ss,
-		(Set.size $ Set.intersection (splitB s1) (splitB s2)) > 0 ]
+		(IntSet.size $ IntSet.intersection (splitB s1) (splitB s2)) > 0 ]
 
 splitA :: Split -> Block
-splitA sp 	| Set.member 1 a = a
+splitA sp 	| IntSet.member 1 a = a
 			| otherwise = b
 	where (a:b:_) = Set.toList sp
 
@@ -52,23 +67,22 @@ joinSplitSystems :: SplitSystem -> SplitSystem -> SplitSystem
 joinSplitSystems ss1 ss2 = Set.union ss1 ss2
 
 mergeSplits :: Split -> Split -> Split
-mergeSplits s1 s2 = sf [ Set.intersection a1 a2, Set.union b1 b2 ]
+mergeSplits s1 s2 = sf [ IntSet.intersection a1 a2, IntSet.union b1 b2 ]
 	where 	(a1:a2:b1:b2:_) = [splitA s1, splitA s2, splitB s1, splitB s2]
 
 allSplits :: Int -> Set.Set Split
-allSplits n = sf [ sf [sp, Set.difference x sp] |
+allSplits n = sf [ sf [sp, IntSet.difference x sp] |
 	k <- [2..n-2],
-	sp <- map sf $ combinations k n ]
-	where x = sf [1 .. n]
+	sp <- map isf $ combinations k n ]
+	where x = isf [1 .. n]
 
-circularSplitSystems :: Int -> Int -> Set.Set SplitSystem
--- circularSplitSystems n k 	| k == 1 = sf [ sf [sp] | sp <- st $ allSplits n ]
---	 							| otherwise = Set.filter (isCircular)
---									(sf $ map (sf) $ combinationsOf k $ st $ allSplits n)
-circularSplitSystems n k = Set.filter (isCircular) 
-	(sf $ map (sf) $ combinationsOf k $ st $ allSplits n)
+css :: Int -> Int -> Set.Set SplitSystem
+css n k = sf [ s | (s,circ) <- zip ss isCirc, circ == True ] where
+		ss = map (sf) $ combinationsOf k $ st $ allSplits n
+		isCirc = (parMap rwhnf) (isCircular) ss
+circularSplitSystems = (Memo.memo2 Memo.integral Memo.integral) css
 
 fVector :: Int -> [Int]
 fVector n = map (Set.size) [ circularSplitSystems n k | k <- [1 .. n*(n-3) `div` 2] ]
 
-main = print $ fVector 6
+main = print $ fVector 5
