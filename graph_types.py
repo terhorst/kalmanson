01dfb6c02b8eb7aff73ee9ef45aa609b5fd624bc
terhorst <terhorst@gmail.com>
2010-12-28 21:33:21 -0800

    code readability

--- graph_types.py
+++ graph_types.py
@@ -1,20 +1,12 @@
-from sage.all import graphs
+from sage.all import graphs, parallel
 from itertools import combinations
 
-def all_graph_types(n, k):
-    edges = filter(lambda (x,y): (x-y) % n not in [1, n-1], combinations(range(n), 2))
+def graph_types(n, k):
     ret = []
+    edges = filter(lambda (x,y): (x-y) % n not in [1, n-1], combinations(range(n), 2))
     for e in combinations(edges, k):
         g = graphs.CycleGraph(n)
         g.add_edges(e)
-        match = False
-        for h in ret:
-            if h.is_isomorphic(g):
-                match = True
-                break
-        if not match:
+        if all(not h.is_isomorphic(g) for h in ret):
             ret.append(g)
-
     return ret
-
-
