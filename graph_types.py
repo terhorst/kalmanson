80bd855b8fbcc8f7042bd31fc2098fb429e5fc37
terhorst <terhorst@gmail.com>
2011-01-02 14:33:00 -0800

    fixed isomorphism bug

--- graph_types.py
+++ graph_types.py
@@ -1,4 +1,4 @@
-from sage.all import graphs, parallel, permutations, Set, floor
+from sage.all import graphs, parallel, permutations, Set, floor, SymmetricGroup, DihedralGroup
 from itertools import combinations
 from memoized import memoized
 from kalmanson import kalmanson_fan, cone_to_split_system
@@ -12,7 +12,7 @@ def graph_types(n, k):
     for e in combinations(edges, k):
         g = graphs.CycleGraph(n)
         g.add_edges((a,b,'split') for a,b in e)
-        if all([not h.is_isomorphic(g) for h in ret]):
+        if not any(is_equivalent(g,h) for h in ret):
             ret.append(g)
     return ret
 
@@ -20,21 +20,40 @@ def graph_types(n, k):
 def match_split_system(ss):
     from graph_types import graph_types, permute_ss, split_edges, edge_to_split
     n = sum(map(len, ss[0]))
-    perms = permutations(range(n))
     k = len(ss)
     gt = graph_types(n,k)
-    return [g for g in gt if
-            any([permute_ss(p, ss) ==
-                Set([edge_to_split(n, e)
-                    for e in split_edges(g)])
-                for p in perms])]
+    perms = SymmetricGroup(n)
+    ss_sig = sorted(map(split_len, ss))
+    for g in gt:
+        gss = graph_to_ss(g)
+        gss_sig = sorted(map(split_len, gss))
+        if gss_sig != ss_sig:
+            continue
+        elif any([permute_ss(p, ss) == gss for p in perms]):
+            return g
+    # return [g for g in gt if any([permute_ss(p, ss) == Set([edge_to_split(n, e)
+    #                for e in split_edges(g)]) for p in perms])]
+
+def split_len(sp):
+    return min(map(len, sp))
+
+def ss_signature(ss):
+    return sorted(map(split_len, ss))
+
+def is_equivalent(g,h):
+    gss,hss = map(graph_to_ss, [g,h])
+    n = len(g.vertices())
+    return any(permute_ss(p,gss) == hss for p in DihedralGroup(n))
+
+@memoized
+def graph_to_ss(g):
+    n = len(g.vertices())
+    return Set([edge_to_split(n,e) for e in split_edges(g)])
 
 def permute_ss(p, ss):
-    n = len(p)
-    m = dict(zip(range(n), p))
     return Set([
             Set(
-                [Set(map(m.get, A)) for A in sp]
+                [Set(map(lambda x: p(x+1) - 1, A)) for A in sp]
                 )
             for sp in ss])
 
