419132d9ff8c89ced2f7e25ccb0f519d4825a29e
terhorst <terhorst@gmail.com>
2011-02-01 11:57:36 -0800

    changed alphabet

--- alphabet.py
+++ alphabet.py
@@ -10,18 +10,19 @@ ALPHA = dict(zip(((1,1,0),(1,0,1),(0,1,1),(1,0,0),
                  map(chr, range(ord("a"), ord("h") + 1))))
 
 I = map(vector, [(1,1,0),(0,1,1),(1,0,1)])
-II = map(vector, [(1,0,0),(0,1,0),(0,0,1),(1,1,1)])
+II = map(vector, [(1,0,0),(0,1,0),(0,0,1)])
+III = [vector((1,1,1))]
 
 S = {
         'S1': (1, 0, 'X'),
-        'S2': (1, 1, 0),
+        # 'S2': (1, 1, 0),
         'S3': (1, 0, 0),
         'S4': (1, 'X', 'X'),
         'S5': (0, 0, 'X'),
-        'S6': (1, 1, 1),
+        # 'S6': (1, 1, 1),
         'S7': (0, 0, 0),
         'S8': (0, 'X', 'X'),
-        'S9': (1, 1, 'X')
+        # 'S9': (1, 1, 'X')
         }
 
 def mat_to_word(ss):
@@ -31,14 +32,21 @@ def __get_mats(n):
     return [ss_to_matrix(n, ss) for ss in circular_ones(n, 3)]
 
 @memoized
-def F(i,j,mats):
-    # mats = __get_mats(n)
-    return sum( sum(vector(S) in M.columns() for S in I)==i and
-                sum(vector(S) in M.columns() for S in II)==j
-                for M in mats )
+def F(i,j,k,mats):
+    return filter(lambda M: sum(vector(S) in M.columns() for S in I)==i and
+                    sum(vector(S) in M.columns() for S in II)==j and
+                    sum(vector(S) in M.columns() for S in III)==k,
+                    mats)
 
 def all_F(mats):
-    return dict(((i,j),F(i,j,mats)) for i in range(3) for j in range(4))
+    d = dict(((i,j,k),F(i,j,k,mats)) for i in range(4) for j in range(5) for k in range(2))
+    print_d(d)
+    return d
+
+def print_d(d):
+    for k,v in d.iteritems():
+        if len(v)>0:
+            print "%s: %i" % (str(k),len(v))
 
 def count_structure(n, sty="S1"):
     pat = S[sty]
@@ -47,12 +55,11 @@ def count_structure(n, sty="S1"):
         if i==1:
             rows.append([(0,) + ((1,)*(n-2)) + (0,)])
         elif i==0:
-            rows.append([(0,)+l+(1,) for l in permutations((0,)*(n-3) + (1,))])
+            rows.append([(0,)*i + (1,) + (0,)*(n-i-2) + (1,)
+                for i in range(1, n-i-1)])
         else:
             rows.append([(0,)+l for l in product([0,1], repeat=n-1)
                 if l.count(1) > 1 and l.count(0) < n-2])
-
-    print rows
 
     ret = []
     proditer = ([p1,p2,p3] for p1,p2,p3 in product(*rows) if p1!=p2 and p2!=p3 and p1!=p3)
@@ -61,3 +68,21 @@ def count_structure(n, sty="S1"):
             ret.append(M)
 
     return ret
+
+def checkit(n):
+    mats = __get_mats(n-1)
+    aF = all_F(mats)
+    print sum((i+j+1) * v for (i,j),v in aF.iteritems())
+    for k in S.keys():
+        print k
+        print len(count_structure(n, k))
+
+def Sij(n):
+    mats = [(M, M.submatrix(0,0,ncols=n-1)) for M in __get_mats(n)]
+    mats_nm1 = __get_mats(n-1)
+    ret = []
+    for M0,M in mats:
+        if any(S in mats_nm1 for S in row_orbit(M)):
+                ret.append(M0)
+    return ret
+
